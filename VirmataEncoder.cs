667b7e49234a3f6a9426cf72ef3e804db2f6e062
jens-a-e <jens@ififelse.net>
2011-10-30 16:42:28 +0100

    Adding full support for sending pin values, implemented reset method and cleaned up unused and overhead functions

--- VirmataEncoder.cs
+++ VirmataEncoder.cs
@@ -1,12 +1,14 @@
+
+
+
 #region usings
 using System;
 using System.ComponentModel.Composition;
 using System.Collections.Generic;
 using System.Text;
+
 using VVVV.PluginInterfaces.V1;
 using VVVV.PluginInterfaces.V2;
-using VVVV.Utils.VColor;
-using VVVV.Utils.VMath;
 using VVVV.Core.Logging;
 #endregion usings
 
@@ -28,6 +30,9 @@ namespace VVVV.Nodes
 		[Input("values")]
 		IDiffSpread<double> PinValues;
 		
+		[Input("PinModes", DefaultEnumEntry = "INPUT")]
+		IDiffSpread<PinModes> PinModeSetup;
+		
 		[Input("report analog pins",IsSingle = true)]
 		IDiffSpread<bool> ReportAnalogPins;
 		
@@ -38,14 +43,11 @@ namespace VVVV.Nodes
 		[Input("Samplerate", MinValue = 0, DefaultValue = 40,IsSingle = true)]
 		IDiffSpread<int> Samplerate;
 		
-		[Input("ReportFirmwareVersion",IsSingle = true, Visibility = PinVisibility.Hidden)]
+		[Input("ReportFirmwareVersion",IsSingle = true, Visibility = PinVisibility.Hidden, IsBang=true)]
 		IDiffSpread<bool> ReportFirmwareVersion;
 		
-		[Input("ResetSystem",IsSingle = true, Visibility = PinVisibility.OnlyInspector)]
+		[Input("Reset",IsSingle = true, Visibility = PinVisibility.Hidden, IsBang=true, DefaultValue = 0)]
 		IDiffSpread<bool> ResetSystem;
-		
-		[Input("PinModes", DefaultEnumEntry = "INPUT")]
-		IDiffSpread<PinModes> PinModeSetup;
 		
 		[Input("SendOnCreate", Visibility = PinVisibility.Hidden, IsSingle = true, DefaultValue = 1)]
 		IDiffSpread<bool> SendOnCreate;
@@ -62,36 +64,32 @@ namespace VVVV.Nodes
 		[Import()]
 		ILogger FLogger;
 		
+		// It is so sparsely documented, but this one is called after load of the node is finished!?
+		public void OnImportsSatisfied()
+		{
+		}
 		
+		/// EVALUATE
 		public void Evaluate(int SpreadMax)
 		{
-			UpdatePinConfiguration();
+			string command_out = UpdatePinConfiguration();
 			
-			string command_out = "";
+			if(ResetSystem.IsChanged && ShouldReset) command_out += GetResetCommand();
 			
-			if(PinModeSetup.IsChanged)
-			{
-				
-				for(int i=0; i<PinModeSetup.SliceCount; i++)
-				{
-					command_out += SetPinModeCommand(PinModeSetup[i],i);
-				}
-			}
+			if (ReportFirmwareVersion.IsChanged) command_out += GetFirmwareVersionCommand();
+			
 			
 			if (PinValues.IsChanged)
-			{
-				command_out += SetPinStates(PinValues,PinModeSetup);
-			}
+				command_out += SetPinStates(PinValues);
 			
 			/// Set Pinreporting for analog pins
-			if (ReportAnalogPins.IsChanged)
-			{
 				// TODO: It should not be a fixed number of pins, later versions
+				// TODO: if spread has only one value, do all, otherwise do given, there are 16!
+			if (ReportAnalogPins.IsChanged || ShouldReset)
 				command_out += SetAnalogPinReportingForRange(6,ReportAnalogPins[0]);
-			}
 			
 			/// Set Pinreporting for digital pins
-			if (ReportDigitalPins.IsChanged)
+			if (ReportDigitalPins.IsChanged || ShouldReset)
 			{
 				// TODO: Check which pin number should be reported and enable only the proper port.
 				// TODO: It could work like: fi spread.slicecount==1 do all, else do specific pins
@@ -100,8 +98,9 @@ namespace VVVV.Nodes
 				command_out += GetDigitalPinReportingCommandForState(ReportDigitalPins[0],ATMegaPorts.PORTD);
 			}
 			
-			if(Samplerate.IsChanged)
+			if(Samplerate.IsChanged || ShouldReset)
 			{
+				// We must shortly trun of the reporting to get immidiate change of rate
 				if (ReportAnalogPins[0])
 				command_out += SetAnalogPinReportingForRange(6,false);
 				
@@ -111,74 +110,127 @@ namespace VVVV.Nodes
 				command_out += SetAnalogPinReportingForRange(6,ReportAnalogPins[0]);
 			}
 			
-			if(ResetSystem.IsChanged)
-			command_out += GetResetCommand();
-			
-			if (ReportFirmwareVersion.IsChanged)
-			command_out += GetFirmwareVersionCommand();
-			
-			/*
-Did something change at all?
-*/
 			bool something_changed = (command_out!="");
 			ChangedOut[0] = something_changed;
-			if(something_changed) FirmataOut[0] = command_out;
+			FirmataOut[0] = command_out;
 		}
 		
 		
 		#region Helper functions
 		
-		Stack<byte> INPUT_PORT_MASKS  = new Stack<byte>();
-		Stack<byte> OUPUT_PORT_MASKS  = new Stack<byte>();
-		Stack<byte> ANALOG_PORT_MASKS = new Stack<byte>();
-		Stack<byte> PWM_PORT_MASKS    = new Stack<byte>();
+		byte[] OUTPUT_PORT_MASKS  = {}; // empty array
+		
+		Queue<byte> INPUT_PORT_MASKS  = new Queue<byte>();
+		Queue<byte> ANALOG_PORT_MASKS = new Queue<byte>();
+		Queue<byte> PWM_PORT_MASKS    = new Queue<byte>();
 		
 		PinModes DEFAULT_PINMODE = PinModes.OUTPUT;
 		
+		double VALUE_THRESHOLD = 0.5;
+		
+		int NUM_OUTPUT_PORTS = 0;
+		int NUM_PORTS = 0; // The total number of ports (AVR PORTS) respective to the number of pins
+		int NUM_PINS  = 0; // The total number of pins addressed by this node
+		
+		bool PINS_CONFIGURED = false;
+		bool PIN_CONFIG_CHANGED = false;
+		
+		bool ShouldReset { get {return ResetSystem[0];} set{} }
+		
+		/// <summary>
+		/// Calculate the total number of pins addressed with this node
+		/// </summary>
+		void UpdatePinCount()
+		{
+			/// Who wins?
+			NUM_PINS  = PinModeSetup.SliceCount >= PinValues.SliceCount ?  PinModeSetup.SliceCount : PinValues.SliceCount;
+			/// calculate the next full divider by 8:
+			NUM_PORTS = NUM_PINS/8 + (NUM_PINS%8==0 ? 0 : 1);
+		}
+		
+		
+		PinModes PinModeForPin(int pin)
+		{
+			return pin<PinModeSetup.SliceCount ? PinModeSetup[pin]:DEFAULT_PINMODE;
+		}
+		
+		string pinmode_cmd_buffer = "";
 		/// <summary>
 		/// Updates the pin masks, number of pins ,etc
 		/// </summary>
-		void UpdatePinConfiguration()
+		string UpdatePinConfiguration()
 		{
-//			if (!PinModeSetup.IsChanged) return;
+			PIN_CONFIG_CHANGED = false;
 			
-			/// calculate the next full divider by 8:
-			int num_pins = PinModeSetup.SliceCount >= PinValues.SliceCount ?  PinModeSetup.SliceCount : PinValues.SliceCount;
-			int port_count  = num_pins/8 + (num_pins%8==0 ? 0 : 1);
-			// byte[] bytes = new byte[port_count];
+			// just a workarround, we are switch to stacks intirely
+			pinmode_cmd_buffer = "";
+			
+			// Do not do it if nothing changed, but do it if the pins have not yet been configured
+			if ((!PinModeSetup.IsChanged && !ShouldReset) && PINS_CONFIGURED) return "";
+			
+			UpdatePinCount();
+			
+			/// TODO Optimize to use actual needed output ports, instead of all
+			NUM_OUTPUT_PORTS = NUM_PORTS;
+			OUTPUT_PORT_MASKS = new byte[NUM_OUTPUT_PORTS];
+			
+			// build the coammnd for pin configuration in parallel
+			Queue<byte> config_command = new Queue<byte>();
 			
 			// allocate memory once
 			byte input_port,output_port,analog_port,pwm_port;
-			for(int i = 0; i<port_count; i++)
+			for(int i = 0; i<NUM_PORTS; i++)
 			{
+				// reset temporary port mask
 				input_port=output_port=analog_port=pwm_port=0x00;
 				
+				// Build the mask
 				for (int bit=0; bit<8; bit++)
 				{
 					int src_index = i*8+bit;
-					PinModes mode = src_index<PinModeSetup.SliceCount ? PinModeSetup[src_index]:DEFAULT_PINMODE;
+					PinModes mode = DEFAULT_PINMODE;
+					
+					/// Set the mode and add to the configure command
+					if(src_index<PinModeSetup.SliceCount)
+					{
+						mode = PinModeSetup[src_index];
+						config_command.Enqueue(FirmataCommands.SETPINMODE);
+						config_command.Enqueue((byte) src_index);
+						config_command.Enqueue((byte) mode);
+					}
+					
 					input_port  |= (byte)((mode == PinModes.INPUT  ? 1:0)<<bit);
 					output_port |= (byte)((mode == PinModes.OUTPUT ? 1:0)<<bit);
 					analog_port |= (byte)((mode == PinModes.ANALOG ? 1:0)<<bit);
 					pwm_port    |= (byte)((mode == PinModes.PWM    ? 1:0)<<bit);
+					
+					/// TODO: If we have a servo, we should send a default config to the command queue
 				}
-				INPUT_PORT_MASKS.Push(input_port);
-				OUPUT_PORT_MASKS.Push(output_port);
-				ANALOG_PORT_MASKS.Push(analog_port);
-				PWM_PORT_MASKS.Push(pwm_port);
+				OUTPUT_PORT_MASKS[i] = output_port;
+				
+				/// These are not needed actually...
+				INPUT_PORT_MASKS.Enqueue(input_port);
+				ANALOG_PORT_MASKS.Enqueue(analog_port);
+				PWM_PORT_MASKS.Enqueue(pwm_port);
+				
 			}
+			
+			
+			// Pin have been configured once:
+			PINS_CONFIGURED = true;
+			
+			// Signal change
+			PIN_CONFIG_CHANGED = true;
+			
+			return Encoder.GetString(config_command.ToArray());
 		}
+		
+		/// Use ANSI Encoding!
+		static Encoding Encoder = Encoding.GetEncoding(1252);
 		
 		/* This is a shortcut to encode byte arrays, which also contain bytes higer than 127 */
 		static string Encode(byte[] bytes) {
-			/// Use ANSI Encoding!
-			return Encoding.GetEncoding(1252).GetString(bytes);
-		}
-		
-		static byte getBitMaskForPinsInMode(PinModes mode)
-		{
-			// TODO Implement!
-			return 0xFF;
+			return Encoder.GetString(bytes);
 		}
 		
 		static string SetPinModeCommand(PinModes mode, int pin)
@@ -188,53 +240,70 @@ Did something change at all?
 				(byte) pin,
 				(byte) mode
 			};
-			return Encode(cmd);
+			return Encoder.GetString(cmd);
 		}
 		
-		byte[] PinSpreadToPorts(ISpread<double> spread, IDiffSpread<PinModes> modes)
+		static int PortIndexForPin(int pin)
 		{
-			int num_ports = spread.SliceCount/8 + (spread.SliceCount%8==0 ? 0 : 1);
-			byte[] bytes = new byte[num_ports];
-			for(int port_index=0; port_index<num_ports; port_index++)
-			{
-				byte port = 0x00;
-				for (int bit=0; bit<8; bit++)
-				{
-					int src_index = port_index*8+bit;
-					double val = src_index<spread.SliceCount ? spread[src_index]:0;
-					/// Only set if pin is configured as output, so get the mode
-					int mode = src_index>=modes.SliceCount ? 0x00 : modes[src_index] == PinModes.OUTPUT ? 0x01:0x00;
-					port |= (byte)((val >= 0.5 && mode>0 ? 1:0)<<bit);
-				}
-				/// TODO: Mask port with PWN PinsMask to
-				bytes[port_index] = port;
-			}
-			return bytes;
+			return pin/8;
 		}
 		
-		string SetPinStates(ISpread<double> values, IDiffSpread<PinModes> modes)
+		string SetPinStates(ISpread<double> values)
 		{
-			// TODO: handle PWN set pins!
+			// TODO: handle PWM set pins!
 			// Maybe this should return an object like Ports.ANALOG, PORTS.DIGITAL
 			
-			byte[] ports = PinSpreadToPorts(values,modes);
+			Queue<byte> cmd = new Queue<byte>();
 			
-			List<byte> cmd = new List<byte>();
-			for(int port=0; port<ports.Length; port++)
+			// get the number of output ports
+			int[] digital_out = new int[OUTPUT_PORT_MASKS.Length];
+			
+			for(int i=0; i<values.SliceCount; i++)
 			{
-				byte LSB, MSB;
-				
-				GetBytesFromValue(ports[port], out MSB, out LSB);
-				
-				byte the_port = ATMegaPorts.getPortForIndex(port);
-				byte writeCommand = (byte)((uint) FirmataCommands.DIGITALMESSAGE | the_port);
-				
-				cmd.Add(writeCommand);
-				cmd.Add(LSB);
-				cmd.Add(MSB);
+				double value = values[i];
+				PinModes mode = PinModeForPin(i);
+				switch(mode)
+				{
+					case PinModes.ANALOG:
+					case PinModes.PWM:
+					case PinModes.SERVO:
+					byte LSB,MSB;
+					value *= mode==PinModes.SERVO ? 180 : 255; // servo is in degrees
+					GetBytesFromValue((int)value,out MSB,out LSB);
+					cmd.Enqueue((byte)(FirmataCommands.ANALOGMESSAGE | i));
+					cmd.Enqueue(LSB);
+					cmd.Enqueue(MSB);
+					break;
+					
+					case PinModes.OUTPUT:
+					int port_index = PortIndexForPin(i);
+					// Break, if we have no ouputports we can get
+					if (port_index >= digital_out.Length) break;
+					
+					int shift = i%8;
+					int state = value >= 0.5 ? 0x01 : 0x00;
+					int port_before = digital_out[port_index];
+					digital_out[port_index] =  ((state << shift) | digital_out[port_index])  & OUTPUT_PORT_MASKS[port_index];
+					break;
+				}
 			}
-			return Encode(cmd.ToArray());
+			
+			/// Write all the output ports to the command buffer
+			for(int port_index=0; port_index<digital_out.Length; port_index++)
+			{
+				byte LSB,MSB;
+				byte atmega_port = ATMegaPorts.getPortForIndex(port_index);
+				GetBytesFromValue(digital_out[port_index],out MSB,out LSB);
+				
+				cmd.Enqueue((byte)(FirmataCommands.DIGITALMESSAGE | atmega_port));
+				cmd.Enqueue(LSB);
+				cmd.Enqueue(MSB);
+				
+			}
+			
+			return Encoder.GetString(cmd.ToArray());
 		}
+		
 		
 		static string GetSamplerateCommand(int rate)
 		{
@@ -450,6 +519,5 @@ Did something change at all?
 	}
 	
 	#endregion
-	
 	
 }
