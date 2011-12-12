6e599912eecb542b7802b019ff6cf0c975d2d9db
jens-a-e <jens@ififelse.net>
2011-12-12 10:14:53 +0100

    Switched to new use new Firmata Namespace for commands, pinmodes, etc
    Added experimental nodes for single en- & decoding of firmatamessage like digitalport.
    
    Minor cleanups and reorganization of order of pins

--- VirmataEncoder.cs
+++ VirmataEncoder.cs
@@ -1,6 +1,6 @@
 #region Copyright notice
 /*
-A Firmata Encoder Plugin for VVVV
+A Firmata Plugin for VVVV
 ----------------------------------
 Encoding control and configuration messages for Firmata enabled MCUs. This
 Plugin encodes to a ANSI string and a byte array, so you can send via any
@@ -11,8 +11,8 @@ Get the source here: https://github.com/jens-a-e/VirmataEncoder
 Any issues should be posted here: https://github.com/jens-a-e/VirmataEncoder/issues
 
 Copyleft 2011
-	Jens Alexander Ewald, http://ififelse.net
-	Chris Engler, http://wirmachenbunt.de
+Jens Alexander Ewald, http://ififelse.net
+Chris Engler, http://wirmachenbunt.de
 
 Inspired by the Sharpduino project by Tasos Valsamidis (LSB and MSB operations)
 See http://code.google.com/p/sharpduino if interested.
@@ -57,10 +57,100 @@ using System.Text;
 using VVVV.PluginInterfaces.V1;
 using VVVV.PluginInterfaces.V2;
 using VVVV.Core.Logging;
+
+using Firmata;
 #endregion usings
 
 namespace VVVV.Nodes
 {
+	#region EXPERIMENTAL NODES!!!
+	/// NOT USED!
+	#region PluginInfo
+	[PluginInfo(Name = "FirmataPort (Encode)",
+	Category = "Devices",
+	Version = "Firmata v2.2",
+	Help = "Represents a Digital Port with 8 I/O s (Protocol v2.2)",
+	Tags = "Devices,Encoders")]
+	#endregion PluginInfo
+	
+	public class FirmataPort : IPluginEvaluate
+	{
+		
+		[Input("Port", DefaultValue = 0)]
+		IDiffSpread<int> Port;
+		
+		[Input("Values")]
+		IDiffSpread<int> Value;
+		
+		[Output("Data")]
+		ISpread<string> Out;
+		
+		public void Evaluate(int SpreadMax)
+		{
+			if (Port.IsChanged || Value.IsChanged)
+			{
+				List<int> val = new List<int>();
+				val.AddRange(Value.GetRange(0,8));
+				Out[0] = Encoding.GetEncoding(1252).GetString(Firmata.FirmataUtils.PortMessage(Port[0],val.ToArray()));
+			}
+		}
+	}
+	
+	
+	#region PluginInfo
+	[PluginInfo(Name = "FirmataPort (Decode)",
+	Category = "Devices",
+	Version = "Firmata v2.2",
+	Help = "Represents a Digital Port with 8 I/O s (Protocol v2.2)",
+	Tags = "Devices,Encoders")]
+	#endregion PluginInfo
+	
+	public class FirmataPortDecode : IPluginEvaluate
+	{
+		
+		[Input("Input")]
+		IDiffSpread<string> In;
+		
+		[Input("PortNumber")]
+		IDiffSpread<int> PortNumber;
+		
+		[Output("Values")]
+		ISpread<int> Values;
+		ISpread<int> CachedValues;
+		
+		[Output("Port")]
+		ISpread<int> Port;
+		
+		public void Evaluate(int SpreadMax)
+		{
+			if (In.IsChanged)
+			{
+				Values.SliceCount = PortNumber.SliceCount * 8; // 8 bits per port
+				Port.SliceCount   = PortNumber.SliceCount;
+				
+				foreach (string msg in In){
+					byte[] data = Encoding.GetEncoding(1252).GetBytes(msg);
+					if(!FirmataUtils.ContainsCommand(data,Command.DIGITALMESSAGE)) {
+						continue; //
+					}
+					int port;
+					int[] values;
+					if (FirmataUtils.DecodePortMessage(data, out port, out values))
+					{
+						Port[0] = port;
+						Values.AssignFrom(values);
+						CachedValues = Values.Clone();
+					} else {
+						Values = CachedValues.Clone();
+					}
+				}
+				
+			}
+		}
+	}
+	
+	#endregion
+	
 	#region PluginInfo
 	[PluginInfo(Name = "FirmataEncode",
 	Category = "Devices",
@@ -78,26 +168,32 @@ namespace VVVV.Nodes
 		IDiffSpread<double> PinValues;
 		
 		[Input("PinModes", DefaultEnumEntry = "INPUT")]
-		IDiffSpread<PinModes> PinModeSetup;
+		IDiffSpread<PinMode> PinModeSetup;
 		
-		[Input("report analog pins",IsSingle = true, DefaultValue = 1)]
+		[Input("ReportAnalogPins",IsSingle = true, DefaultValue = 1)]
 		IDiffSpread<bool> ReportAnalogPins;
 		
 		[Input("ReportDigitalPins",IsSingle = true, DefaultValue = 1)]
 		IDiffSpread<bool> ReportDigitalPins;
 		
+		[Input("Reset",IsSingle = true, IsBang=true, DefaultValue = 0)]
+		IDiffSpread<bool> ResetSystem;
+		
 		//// Use a default SamplingRate of 40ms
-		[Input("Samplerate", MinValue = 0, DefaultValue = 40,IsSingle = true)]
+		[Input("Samplerate", MinValue = 0, DefaultValue = 40,IsSingle = true, Visibility = PinVisibility.Hidden)]
 		IDiffSpread<int> Samplerate;
+		
+		[Input("SendOnCreate", Visibility = PinVisibility.Hidden, IsSingle = true, DefaultValue = 1)]
+		IDiffSpread<bool> SendOnCreate;
 		
 		[Input("ReportFirmwareVersion",IsSingle = true, Visibility = PinVisibility.OnlyInspector, IsBang=true)]
 		IDiffSpread<bool> ReportFirmwareVersion;
 		
-		[Input("Reset",IsSingle = true, Visibility = PinVisibility.Hidden, IsBang=true, DefaultValue = 0)]
-		IDiffSpread<bool> ResetSystem;
+		[Input("AnalogInputCount",DefaultValue = 6, Visibility = PinVisibility.OnlyInspector, IsSingle = true)]
+		ISpread<int> analogInputCount;
 		
-		[Input("SendOnCreate", Visibility = PinVisibility.Hidden, IsSingle = true, DefaultValue = 1)]
-		IDiffSpread<bool> SendOnCreate;
+		[Input("DigitalInputCount",DefaultValue = 14, Visibility = PinVisibility.OnlyInspector, IsSingle = true)]
+		ISpread<int> digitalInputCount;
 		
 		///
 		/// OUTPUT
@@ -124,11 +220,10 @@ namespace VVVV.Nodes
 			// Clear the buffer before everey run
 			CommandBuffer.Clear();
 			
-			if(ResetSystem.IsChanged && ShouldReset)
-			GetResetCommand();
-			
 			if(PinModeSetup.IsChanged || ShouldReset || !PINS_CONFIGURED)
 			UpdatePinConfiguration();
+			
+			
 			
 			if (PinModeSetup.IsChanged || PinValues.IsChanged || ShouldReset)
 			SetPinStates(PinValues);
@@ -137,27 +232,29 @@ namespace VVVV.Nodes
 			// TODO: It should not be a fixed number of pins, later versions
 			// TODO: if spread has only one value, do all, otherwise do given, there are 16!
 			if (ReportAnalogPins.IsChanged || ShouldReset)
-			SetAnalogPinReportingForRange(16,ReportAnalogPins[0]);
+			SetAnalogPinReportingForRange(analogInputCount[0],ReportAnalogPins[0]);
 			
 			/// Set Pinreporting for digital pins
 			if (ReportDigitalPins.IsChanged || ShouldReset)
 			{
 				// TODO: Check which pin number should be reported and enable only the proper port.
-				// TODO: It could work like: fi spread.slicecount==1 do all, else do specific pins
-				GetDigitalPinReportingCommandForState(ReportDigitalPins[0],ATMegaPorts.PORTB);
-				GetDigitalPinReportingCommandForState(ReportDigitalPins[0],ATMegaPorts.PORTC);
-				GetDigitalPinReportingCommandForState(ReportDigitalPins[0],ATMegaPorts.PORTD);
+				// TODO: It could work like: if spread.slicecount==1 do all, else do specific pins
+				GetDigitalPinReportingCommandForState(ReportDigitalPins[0],Port.PORTB);
+				GetDigitalPinReportingCommandForState(ReportDigitalPins[0],Port.PORTD);
 			}
 			
 			if(Samplerate.IsChanged || ShouldReset)
 			{
 				// We must shortly trun of the reporting to get immidiate change of rate
-				if (ReportAnalogPins[0]) SetAnalogPinReportingForRange(6,false);
+				if (ReportAnalogPins[0]) SetAnalogPinReportingForRange(analogInputCount[0],false);
 				GetSamplerateCommand(Samplerate[0]);
-				if (ReportAnalogPins[0]) SetAnalogPinReportingForRange(6,true);
+				if (ReportAnalogPins[0]) SetAnalogPinReportingForRange(analogInputCount[0],true);
 			}
 			
 			if (ReportFirmwareVersion.IsChanged) GetFirmwareVersionCommand();
+			
+			if(ResetSystem.IsChanged && ShouldReset)
+			GetResetCommand();
 			
 			ChangedOut[0] = CommandBuffer.Count>0;
 			RawOut[0]     = CommandBuffer.ToArray();
@@ -165,14 +262,14 @@ namespace VVVV.Nodes
 		}
 		
 		
-		#region Member definitions & helper functions
+		#region Helper Functions
 		
 		/// Use ANSI Encoding for the Encoder
 		static Encoding Encoder = Encoding.GetEncoding(1252);
 		
 		byte[] OUTPUT_PORT_MASKS  = {}; // empty array
 		
-		PinModes DEFAULT_PINMODE = PinModes.OUTPUT;
+		PinMode DEFAULT_PINMODE = PinMode.OUTPUT;
 		
 		double VALUE_THRESHOLD = 0.5;
 		
@@ -183,6 +280,7 @@ namespace VVVV.Nodes
 		bool PINS_CONFIGURED = false;
 		bool PIN_CONFIG_CHANGED = false;
 		
+		// Make a shortcut for ResetSystem[0]
 		bool ShouldReset { get {return ResetSystem[0];} set{} }
 		
 		/// <summary>
@@ -197,7 +295,7 @@ namespace VVVV.Nodes
 		}
 		
 		
-		PinModes PinModeForPin(int pin)
+		PinMode PinModeForPin(int pin)
 		{
 			return pin<PinModeSetup.SliceCount ? PinModeSetup[pin]:DEFAULT_PINMODE;
 		}
@@ -226,18 +324,18 @@ namespace VVVV.Nodes
 				for (int bit=0; bit<8; bit++)
 				{
 					int src_index = i*8+bit;
-					PinModes mode = DEFAULT_PINMODE;
+					PinMode mode = DEFAULT_PINMODE;
 					
 					/// Set the mode and add to the configure command
 					if(src_index<PinModeSetup.SliceCount)
 					{
 						mode = PinModeSetup[src_index];
-						CommandBuffer.Enqueue(FirmataCommands.SETPINMODE);
+						CommandBuffer.Enqueue(Command.SETPINMODE);
 						CommandBuffer.Enqueue((byte) src_index);
 						CommandBuffer.Enqueue((byte) mode);
 					}
 					
-					output_port |= (byte)((mode == PinModes.OUTPUT ? 1:0)<<bit);
+					output_port |= (byte)((mode == PinMode.OUTPUT ? 1:0)<<bit);
 				}
 				OUTPUT_PORT_MASKS[i] = output_port;
 			}
@@ -251,9 +349,9 @@ namespace VVVV.Nodes
 		
 		
 		
-		void SetPinModeCommand(PinModes mode, int pin)
+		void SetPinModeCommand(PinMode mode, int pin)
 		{
-			CommandBuffer.Enqueue(FirmataCommands.SETPINMODE);
+			CommandBuffer.Enqueue(Command.SETPINMODE);
 			CommandBuffer.Enqueue((byte) pin);
 			CommandBuffer.Enqueue((byte) mode);
 		}
@@ -266,26 +364,27 @@ namespace VVVV.Nodes
 		void SetPinStates(ISpread<double> values)
 		{
 			// get the number of output ports
+			// FIXME: Make MAX_PORTS avaiable through Firmata
 			int[] digital_out = new int[OUTPUT_PORT_MASKS.Length];
 			
 			for(int i=0; i<values.SliceCount; i++)
 			{
 				double value = values[i];
-				PinModes mode = PinModeForPin(i);
+				PinMode mode = PinModeForPin(i);
 				switch(mode)
 				{
-					case PinModes.ANALOG:
-					case PinModes.PWM:
-					case PinModes.SERVO:
+					case PinMode.ANALOG:
+					case PinMode.PWM:
+					case PinMode.SERVO:
 					byte LSB,MSB;
-					value *= mode==PinModes.SERVO ? 180 : 255; // servo is in degrees
-					GetBytesFromValue((int)value,out MSB,out LSB);
-					CommandBuffer.Enqueue((byte)(FirmataCommands.ANALOGMESSAGE | i));
+					value *= mode==PinMode.SERVO ? 180 : 255; // servo is in degrees
+					FirmataUtils.GetBytesFromValue((int)value,out MSB,out LSB);
+					CommandBuffer.Enqueue((byte)(Command.ANALOGMESSAGE | i));
 					CommandBuffer.Enqueue(LSB);
 					CommandBuffer.Enqueue(MSB);
 					break;
 					
-					case PinModes.OUTPUT:
+					case PinMode.OUTPUT:
 					int port_index = PortIndexForPin(i);
 					// Break, if we have no ouputports we can get
 					if (port_index >= digital_out.Length) break;
@@ -302,10 +401,10 @@ namespace VVVV.Nodes
 			for(int port_index=0; port_index<digital_out.Length; port_index++)
 			{
 				byte LSB,MSB;
-				byte atmega_port = ATMegaPorts.getPortForIndex(port_index);
-				GetBytesFromValue(digital_out[port_index],out MSB,out LSB);
+				byte atmega_port = (byte) port_index; //Array.GetValues(Port)[port_index];
+				FirmataUtils.GetBytesFromValue(digital_out[port_index],out MSB,out LSB);
 				
-				CommandBuffer.Enqueue((byte)(FirmataCommands.DIGITALMESSAGE | atmega_port));
+				CommandBuffer.Enqueue((byte)(Command.DIGITALMESSAGE | atmega_port));
 				CommandBuffer.Enqueue(LSB);
 				CommandBuffer.Enqueue(MSB);
 				
@@ -316,27 +415,27 @@ namespace VVVV.Nodes
 		void GetSamplerateCommand(int rate)
 		{
 			byte lsb,msb;
-			GetBytesFromValue(rate,out msb,out lsb);
-			CommandBuffer.Enqueue(FirmataCommands.SYSEX_START);
-			CommandBuffer.Enqueue(FirmataCommands.SAMPLING_INTERVAL);
+			FirmataUtils.GetBytesFromValue(rate,out msb,out lsb);
+			CommandBuffer.Enqueue(Command.SYSEX_START);
+			CommandBuffer.Enqueue(Command.SAMPLING_INTERVAL);
 			CommandBuffer.Enqueue(lsb);
 			CommandBuffer.Enqueue(msb);
-			CommandBuffer.Enqueue(FirmataCommands.SYSEX_END);
+			CommandBuffer.Enqueue(Command.SYSEX_END);
 		}
 		
 		
 		/// Query Firmware Name and Version
 		void GetFirmwareVersionCommand()
 		{
-			CommandBuffer.Enqueue(FirmataCommands.SYSEX_START);
-			CommandBuffer.Enqueue(FirmataCommands.REPORT_FIRMWARE_VERSION_NUM);
-			CommandBuffer.Enqueue(FirmataCommands.SYSEX_END);
+			CommandBuffer.Enqueue(Command.SYSEX_START);
+			CommandBuffer.Enqueue(Command.REPORT_FIRMWARE_VERSION);
+			CommandBuffer.Enqueue(Command.SYSEX_END);
 		}
 		
 		void GetAnalogPinReportingCommandForState(bool state,int pin)
 		{
 			byte val = (byte) (state ? 0x01 : 0x00);
-			CommandBuffer.Enqueue((byte)(FirmataCommands.TOGGLEANALOGREPORT|pin));
+			CommandBuffer.Enqueue((byte)(Command.TOGGLEANALOGREPORT|pin));
 			CommandBuffer.Enqueue(val);
 		}
 		
@@ -346,171 +445,23 @@ namespace VVVV.Nodes
 			GetAnalogPinReportingCommandForState(state,i);
 		}
 		
-		void GetDigitalPinReportingCommandForState(bool state,int port)
+		void GetDigitalPinReportingCommandForState(bool state,Port port)
 		{
 			byte val = (byte) (state ? 0x01 : 0x00);
-			CommandBuffer.Enqueue((byte)(FirmataCommands.TOGGLEDIGITALREPORT|port));
+			CommandBuffer.Enqueue((byte)(Command.TOGGLEDIGITALREPORT|(int)port));
 			CommandBuffer.Enqueue(val);
 		}
 		
 		void GetResetCommand()
 		{
-			CommandBuffer.Enqueue(FirmataCommands.SYSEX_START);
-			CommandBuffer.Enqueue(FirmataCommands.RESET);
-			CommandBuffer.Enqueue(FirmataCommands.SYSEX_END);
-		}
-		
-		/// <summary>
-		/// Get the integer value that was sent using the 7-bit messages of the firmata protocol
-		/// </summary>
-		static int GetValueFromBytes(byte MSB, byte LSB)
-		{
-			int tempValue = MSB & 0x7F;
-			tempValue = tempValue << 7;
-			tempValue = tempValue | (LSB & 0x7F);
-			return tempValue;
-		}
-		
-		/// <summary>
-		/// Split an integer value to two 7-bit parts so it can be sent using the firmata protocol
-		/// </summary>
-		static void GetBytesFromValue(int value, out byte MSB, out byte LSB)
-		{
-			LSB = (byte)(value & 0x7F);
-			MSB = (byte)((value >> 7) & 0x7F);
-		}
-		
-		/// <summary>
-		/// Send an array of boolean values indicating the state of each individual
-		/// pin and get a byte representing a port
-		/// </summary>
-		public static byte GetPortFromPinValues(bool[] pins)
-		{
-			byte port = 0;
-			for (int i = 0; i < pins.Length; i++)
-			{
-				port |= (byte) ((pins[i] ? 1 : 0) << i);
-			}
-			return port;
+			CommandBuffer.Enqueue(Command.SYSEX_START);
+			CommandBuffer.Enqueue(Command.RESET);
+			CommandBuffer.Enqueue(Command.SYSEX_END);
 		}
 		
 		#endregion
 		
 	}
 	
-	#region DEFINITIONS
-	public static class FirmataCommands
-	{
-		/// <summary>
-		/// The command that toggles the continuous sending of the
-		/// analog reading of the specified pin
-		/// </summary>
-		public const byte TOGGLEANALOGREPORT = 0xC0;
-		/// <summary>
-		/// The distinctive value that states that this message is an analog message.
-		/// It comes as a report for analog in pins, or as a command for PWM
-		/// </summary>
-		public const byte ANALOGMESSAGE = 0xE0;
-		/// <summary>
-		/// The command that toggles the continuous sending of the
-		/// digital state of the specified port
-		/// </summary>
-		public const byte TOGGLEDIGITALREPORT = 0xD0;
-		
-		/// <summary>
-		/// The distinctive value that states that this message is a digital message.
-		/// It comes as a report or as a command
-		/// </summary>
-		public const byte DIGITALMESSAGE = 0x90;
-		
-		/// <summary>
-		/// A command to change the pin mode for the specified pin
-		/// </summary>
-		public const byte SETPINMODE = 0xF4;
-		
-		/// <summary>
-		/// Sysex start command
-		/// </summary>
-		public const byte SYSEX_START = 0xF0;
-		
-		/// <summary>
-		/// Sysex end command
-		/// </summary>
-		public const byte SYSEX_END = 0xF7;
-		
-		/// <summary>
-		/// Report the Firmware version
-		/// </summary>
-		public const byte REPORT_FIRMWARE_VERSION_NUM = 0x79;
-		
-		/// <summary>
-		/// Reset System Command
-		/// </summary>
-		public const byte RESET = 0xFF;
-		
-		/// <summary>
-		/// Set Samplingrate Command
-		/// </summary>
-		public const byte SAMPLING_INTERVAL = 0x7A;
-	}
-	
-	public enum PinModes
-	{
-		/// <summary>
-		/// Pinmode INPUT
-		/// </summary>
-		INPUT = 0x00,
-		
-		/// <summary>
-		/// Pinmode OUTPUT
-		/// </summary>
-		OUTPUT = 0x01,
-		
-		/// <summary>
-		/// Pinmode ANALOG (This is not implemented in the standard firmata program)
-		/// </summary>
-		ANALOG = 0x02,
-		
-		/// <summary>
-		/// Pinmode PWM
-		/// </summary>
-		PWM = 0x03,
-		
-		/// <summary>
-		/// Pinmode SERVO
-		/// </summary>
-		SERVO = 0x04,
-	}
-	
-	public static class ATMegaPorts
-	{
-		/// <summary>
-		/// This port represents digital pins 0..7. Pins 0 and 1 are reserved for communication
-		/// </summary>
-		public const byte PORTD = 0;
-		
-		/// <summary>
-		/// This port represents digital pins 8..13. 14 and 15 are for the crystal
-		/// </summary>
-		public const byte PORTB = 1;
-		
-		/// <summary>
-		/// This port represents analog input pins 0..5
-		/// </summary>
-		public const byte PORTC = 2;
-		
-		public static byte getPortForIndex(int index)
-		{
-			switch (index)
-			{
-				case 0: return PORTD;
-				case 1: return PORTB;
-				case 2: return PORTC;
-			}
-			return 0;
-		}
-	}
-	
-	#endregion
 	
 }
